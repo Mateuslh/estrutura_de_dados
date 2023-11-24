124c8463de53aeb85c53fd46e754f49b9d15b5b0
rafaelrgaidzinski <rafaelgaidzinski@gmail.com>
2023-11-24 18:16:50 -0300

    Revert "Teste"
    This reverts commit 1ed1f2a718fa7912877f734c19230e822d2f7ab7.

--- domain/Grafo.py
+++ domain/Grafo.py
@@ -2,8 +2,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Grafo:
-    def __init__(self):
+    def __init__(self, arestas):
         self.__Grafo = nx.Graph()
+
+        for aresta in arestas:
+            v1, v2, custo = aresta
+            self.__Grafo.add_edge(v1, v2, weight=custo)
 
     @property
     def vertices(self):
@@ -12,7 +16,6 @@ class Grafo:
     @property
     def arestas(self):
         return list(self.__Grafo.edges)
-    
     def busca_vertices_adjacentes(self, vertice):
         if self.vertice_existe(vertice):
             return list(self.__Grafo.neighbors(vertice))
@@ -47,10 +50,6 @@ class Grafo:
     def remove_vertice(self, rotulo_vertice):
         self.__Grafo.remove_node(rotulo_vertice)
 
-    def retorna_vertice(self, rotulo_vertice):
-        index = self.vertices.index(rotulo_vertice)
-        return self.vertices[index]
-
     def aresta_existe(self,v1,v2):
         return self.__Grafo.has_edge(v1, v2)
 
@@ -63,17 +62,3 @@ class Grafo:
         nx.draw(self.__Grafo, pos, with_labels=True)
         nx.draw_networkx_edge_labels(self.__Grafo, pos, edge_labels=labels)
         plt.show()
-
-grafo = Grafo()
-
-grafo.adiciona_vertice("Vila Natal")
-grafo.adiciona_vertice("Capão Redondo")
-grafo.adiciona_vertice("Santo Amaro")
-
-grafo.adiciona_aresta(grafo.retorna_vertice("Santo Amaro"), grafo.retorna_vertice("Vila Natal"), 6)
-grafo.adiciona_aresta(grafo.retorna_vertice("Santo Amaro"), grafo.retorna_vertice("Capão Redondo"), 4)
-
-print(grafo.vertices)
-print(grafo.arestas)
-
-grafo.mostra_grafo()
